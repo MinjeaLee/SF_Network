1447c7e407f778514b3b04415aa2388ebdc28e8f
MinjeaLee <gabe0529@naver.com>
2022-05-17 20:14:44 +0900

    arp-test assignment wee3

---
+++ assignments/week_3/main.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <pcap.h>
+#include "ethhdr.h"
+#include "arphdr.h"
+
+#pragma pack(push, 1)
+struct EthArpPacket final {
+	EthHdr eth_;
+	ArpHdr arp_;
+};
+#pragma pack(pop)
+
+void usage() {
+	printf("syntax: arp-test <interface> <victim ip> <gateway ip>\n");
+	printf("arp-test wlan0 192.168.10.2 192.168.10.1\n");
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 4) {
+		usage();
+		return -1;
+	}
+
+	char* dev = argv[1];
+	char* victim_ip = argv[2];
+	char* gateway_ip = argv[3];
+
+
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t* handle = pcap_open_live(dev, 0, 0, 0, errbuf);
+	if (handle == nullptr) {
+		fprintf(stderr, "couldn't open device %s(%s)\n", dev, errbuf);
+		return -1;
+	}
+
+	EthArpPacket packet;
+
+	packet.eth_.dmac_ = Mac("00:00:00:00:00:00"); // To get the victim's Mac address
+	packet.eth_.smac_ = Mac("00:0c:29:84:a7:96");
+	packet.eth_.type_ = htons(EthHdr::Arp);
+
+	packet.arp_.hrd_ = htons(ArpHdr::ETHER);
+	packet.arp_.pro_ = htons(EthHdr::Ip4);
+	packet.arp_.hln_ = Mac::SIZE;
+	packet.arp_.pln_ = Ip::SIZE;
+	packet.arp_.op_ = htons(ArpHdr::Request);
+	packet.arp_.smac_ = Mac("00:0c:29:84:a7:96");
+	packet.arp_.sip_ = htonl(Ip("192.168.0.250"));
+	packet.arp_.tmac_ = Mac("E8:84:A5:4B:83:22");
+	packet.arp_.tip_ = htonl(Ip("192.168.0.68"));
+
+	int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
+	if (res != 0) {
+		fprintf(stderr, "pcap_sendpacket return %d error=%s\n", res, pcap_geterr(handle));
+	}
+
+	pcap_close(handle);
+}
